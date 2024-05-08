4c92206143be733c803f30ec1e8b83fa12171f0f
mefedraw <fyodor.dubkov@gmail.com>
2024-05-08 21:36:34 +0300

    UserFundsTable migrations added

---
+++ TgAppCrates.DataAccess/Migrations/20240508183309_UserFundsTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TgAppCrates.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserFundsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFunds",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TgId = table.Column<string>(type: "text", nullable: false),
+                    Funds = table.Column<decimal>(type: "numeric(20,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFunds", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFunds");
+        }
+    }
+}
